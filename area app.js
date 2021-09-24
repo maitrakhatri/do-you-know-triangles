9b1c9643a9319bfe797a2625c55146d6ba493758
maitrakhatri <maitrakhatri@gmail.com>
2021-09-24 11:45:56 +0530

    Area FULL COMPLETE

---
+++ area app.js
@@ -0,0 +1,41 @@
+var input1 = document.querySelector("#input-1")
+var input2 = document.querySelector("#input-2")
+var checkButton = document.querySelector("#checkbutton")
+var result = document.querySelector("#result")
+
+function showMessage(message) {
+    result.style.display = "block";
+    result.innerText = message;
+}
+
+function areaCal(input1, input2) {
+    var value1 = Number(input1.value);
+    var value2 =  Number(input2.value);
+    var product = value1 * value2;
+    // console.log(sum)
+    var area = 0.5*product;
+    // console.log(hypo);
+    return area;
+}
+
+checkButton.addEventListener("click", function clickHandler() {
+
+    result.style.display = "none";
+
+    if (input1.value == "" || input2.value == "" || input1.value < 0 || input2.value < 0) {
+        showMessage("Please enter all Positive values");
+        
+    }
+    else {
+        var area = areaCal(input1, input2);
+        showMessage("The area of the triangle is " + area + "cm^2")
+    }
+
+    
+
+});
+
+
+
+
+
