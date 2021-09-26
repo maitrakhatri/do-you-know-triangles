79bc883080bae320e93d9e01b7c66ba647fde04f
maitrakhatri <maitrakhatri@gmail.com>
2021-09-26 14:50:41 +0530

    solved the "showing score even if not attempted all question" problem

--- quiz app.js
+++ quiz app.js
@@ -2,9 +2,36 @@ var quizForm = document.querySelector("#quizform")
 var submitButton = document.querySelector("#submit-btn")
 var result = document.querySelector("#result")
 
-var correctAnswers = ["scalene triangle", "right angle triangle", "1 ⁄ 2 × bh"];
+var correctAnswers = ["scalene triangle", "right angle triangle", "1 ⁄ 2 × bh", "90"];
+
+var radioButton = document.getElementsByTagName('input');
+
+function showMessage(message) {
+    result.style.display = "block";
+    result.innerText = message;
+}
+
+function submitButtonHandler(){
+   
+    var counter = 0;
+    for(i=0; i < radioButton.length; i++){
+    
+        if (radioButton[i].type === 'radio' && radioButton[i].checked) {
+            counter = counter+1;
+        }
+    }
+
+    if(counter == radioButton.length/3) {
+        calculateScore();
+    }
+    else {
+        showMessage("Attempt all the question")
+    }
+}
+
 
 function calculateScore(){
+    
     var score = 0;
     var index = 0;
 
@@ -16,8 +43,7 @@ function calculateScore(){
         }
         index = index +1;
     }
-
-    result.innerText = "Your Score is " + score;
+    showMessage("Your Score is: " + score);
 }
 
-submitButton.addEventListener('click', calculateScore)
+submitButton.addEventListener('click', submitButtonHandler)
